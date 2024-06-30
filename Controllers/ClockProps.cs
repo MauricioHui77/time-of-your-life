855a5f8295b5c0227132dc5f8eed6da39927072f
Mauricio Huiza <mauriciohuiza13@gmail.com>
2024-06-30 18:36:30 -0500

    feat: include preset id and clock title

--- Controllers/ClockProps.cs
+++ Controllers/ClockProps.cs
@@ -1,10 +1,12 @@
 namespace time.Controllers;
 
 public class ClockProps {
+  public Guid PresetId {get; set;}
   public string FontFamily {get; set;} = "courier";
   public int[] AvailableFontSizes {get; }  = new[] { 12, 24, 48, 64 };
   public int TitleFontSize {get; set;} = 64;
   public int ClockFontSize {get ; set;} = 48;
   public bool BlinkColons {get; set;} = true;
   public string FontColor {get; set;} = "black";
+  public string ClockTitle {get; set;} = "Time of your life";
 }
